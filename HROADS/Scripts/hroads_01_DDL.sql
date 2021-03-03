1507e36f3a9495c0a1a5befbe7d57dc1dbf73ee7
Erika Leite <71355947+ErikaLeite@users.noreply.github.com>
2021-03-03 11:35:16 -0300

    Update hroads_01_DDL.sql

--- HROADS/Scripts/hroads_01_DDL.sql
+++ HROADS/Scripts/hroads_01_DDL.sql
@@ -18,16 +18,16 @@ CREATE TABLE Habilidade
 
 CREATE TABLE Classe
 (
-	 IdClasse				INT PRIMARY KEY IDENTITY
-	,NomeClasse				VARCHAR(200) NOT NULL
+	 IdClasse		INT PRIMARY KEY IDENTITY
+	,NomeClasse		VARCHAR(200) NOT NULL
 );
 
 
 CREATE TABLE ClasseHabilidade
 (
-	 IdClasseHabilidade		INT PRIMARY KEY IDENTITY
-	,IdHabilidade			INT FOREIGN KEY REFERENCES Habilidade (IdHabilidade)
-	,IdClasse				INT FOREIGN KEY REFERENCES Classe     (IdClasse)
+	 IdClasseHabilidade	INT PRIMARY KEY IDENTITY
+	,IdHabilidade		INT FOREIGN KEY REFERENCES Habilidade (IdHabilidade)
+	,IdClasse		INT FOREIGN KEY REFERENCES Classe     (IdClasse)
 );
 
 
@@ -35,9 +35,9 @@ CREATE TABLE Personagem
 (
 	IdPersonagem		INT PRIMARY KEY IDENTITY
 	,NomePersonagem		VARCHAR(200) NOT NULL
-	,IdClasse			INT FOREIGN KEY REFERENCES Classe (IdClasse)
-	,VidaMáxima			VARCHAR(100) NOT NULL
-	,ManaMáxima			VARCHAR(80) NOT NULL
+	,IdClasse		INT FOREIGN KEY REFERENCES Classe (IdClasse)
+	,VidaMáxima		VARCHAR(100) NOT NULL
+	,ManaMáxima		VARCHAR(80) NOT NULL
 	,DataAtualizacao	DATE
 	,DataCriacao		DATE
 );
